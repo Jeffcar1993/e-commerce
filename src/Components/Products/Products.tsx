24cd31b1b9008a71278a443860e7b067920c9056
Jeffcar1993 <jphoenix.07@hotmail.com>
2025-02-07 20:14:43 -0500

    UPDATE componente productos

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,9 +1,25 @@
+import styles from "./Products.module.css";
+import data from "../../data/data.json";
 
 
 const Products = () => {
+
   return (
     <div>
-        <h1>Products</h1>
+
+        <h1>Productos</h1>
+
+        <div className={styles.contenedorProductos}>
+          { data.map((prod) => (
+            <div className={styles.producto} key={prod.id}>
+              <img className={styles.imagen} src={prod.imagen} alt={prod.titulo} />
+              <h2 className={styles.title}>{prod.titulo}</h2>
+              <p className={styles.info}>Cat: {prod.categoria}</p>
+              <p className={styles.info}>$ {prod.precio}</p>
+            </div>
+          ))}
+        </div>
+
     </div>
   )
 }
